cbd5e84f6c17496cadf8e7dd45f9b044537a4258
ggavi2000 <ggavi2000@naver.com>
2019-08-13 20:59:24 +0900

    로딩바 추가

--- client/src/App.js
+++ client/src/App.js
@@ -12,51 +12,62 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 // 가로 스크롤바 (root 클래스: 너비 100%, 위쪽으로 여백을 3의 가중치만큼 주고, x축으로 overflow 발생시키기)
-const styles = theme => ( {
+const styles = theme => ({
   root: {
     width: '100%',
     marginTop: theme.spacing.unit * 3,
     overflowX: "auto"
   },
   table: {
-    minWidth:1080
+    minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2  // margin을 준다
   }
 })
 
 
 
-
 class App extends Component {
-  
   // state: 서버로부터 고객 정보를 받아와야 하므로 데이터는 항상 변할 수 있다.
   state = {
-    customers: ""
+    customers: "",
+    completed: 0  // 로딩바가 0%부터 100%까지 올라가도록 하기 위해
   }
 
+  // [서버 연동]
   // Api 서버에 접근해서 데이터 받아오는 곳.
   // 리액트의 컴포넌트는 라이브러리이므로 생명주기가 존재한다.
   // 모든 컴포넌트가 마운트가 끝날 때 실행
   componentDidMount() {
-    this.callApi()
-
-    // callApi 함수로부터 고객 목록 데이터를 받아가지고 state에 넣어준다.
-    // callApi 함수가 리턴한 body라는 변수를 res로 받는다.
-      .then(res => this.setState({customers: res}))
-
-      // 오류가 발생한 경우
-      .catch(err => console.log(err));
+    this.timer = setInterval(this.progress, 20);  // 로딩바: 0.02초마다 1번씩 progress 함수 실행
+    this.callApi()     // callApi 함수로부터 고객 목록 데이터를 받아가지고 state에 넣어준다.
+      .then(res => this.setState({ customers: res }))   // callApi 함수가 리턴한 body라는 변수를 res로 받는다.
+      .catch(err => console.log(err));                // 오류가 발생한 경우
   }
-  
+
   // 비동기적 어떠한 내용을 수행
-  callApi = async() => {
+  callApi = async () => {
     const response = await fetch('/api/customers');  // 접속하고자 하는 api의 주소를 넣음 (테스트: 로컬호스트의 api/customers에 접근)
 
     // 고객 목록이 json 형태로 출력되면 body라는 변수에 넣어준다.
     const body = await response.json();
     return body;  // 고객 목록 데이터를 전달
   }
+
+  // 로딩바
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 })  // 100% 되면 다시 0으로
+  }
+
+  ///////////////////
+  // ↑ 서버 연동 끝 //
+  ///////////////////
+
 
   render() {
     const { classes } = this.props;
@@ -76,12 +87,19 @@ class App extends Component {
           </TableHead>
 
           <TableBody>
-            {/* 만약 this.state.customers의 값이 존재하는 경우 실행, 아닐 경우 공백을 출력(서버가 데이터를 바로 못 받는 경우가 있으므로 예외처리를 해줌) */}
-            { this.state.customers ? this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); 
-            }) : ""}
+            {/* 만약 this.state.customers의 값이 존재하는 경우 실행, 아닐 경우 로딩바 실행 */}
+            {this.state.customers ? this.state.customers.map(c => {
+              return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+            }) :
+              // 로딩바 실행
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+              </TableRow>
+            }
           </TableBody>
         </Table>
-
       </Paper>
     );
   }
