0a9be024460a75375b7912894be175f8d5c2d48d
ggavi2000 <ggavi2000@naver.com>
2019-08-13 20:35:16 +0900

    Get Customers REST API Module 추가

--- client/src/App.js
+++ client/src/App.js
@@ -26,37 +26,38 @@ const styles = theme => ( {
 })
 
 
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '이름1',
-    'birthday': '생일1',
-    'gender': '성별1',
-    'job': '직업1'
-  },
-
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '이름2',
-    'birthday': '생일2',
-    'gender': '성별2',
-    'job': '직업2'
-  },
-
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '이름3',
-    'birthday': '생일3',
-    'gender': '성별3',
-    'job': '직업3'
-  },
-]
 
 
 class App extends Component {
+  
+  // state: 서버로부터 고객 정보를 받아와야 하므로 데이터는 항상 변할 수 있다.
+  state = {
+    customers: ""
+  }
+
+  // Api 서버에 접근해서 데이터 받아오는 곳.
+  // 리액트의 컴포넌트는 라이브러리이므로 생명주기가 존재한다.
+  // 모든 컴포넌트가 마운트가 끝날 때 실행
+  componentDidMount() {
+    this.callApi()
+
+    // callApi 함수로부터 고객 목록 데이터를 받아가지고 state에 넣어준다.
+    // callApi 함수가 리턴한 body라는 변수를 res로 받는다.
+      .then(res => this.setState({customers: res}))
+
+      // 오류가 발생한 경우
+      .catch(err => console.log(err));
+  }
+  
+  // 비동기적 어떠한 내용을 수행
+  callApi = async() => {
+    const response = await fetch('/api/customers');  // 접속하고자 하는 api의 주소를 넣음 (테스트: 로컬호스트의 api/customers에 접근)
+
+    // 고객 목록이 json 형태로 출력되면 body라는 변수에 넣어준다.
+    const body = await response.json();
+    return body;  // 고객 목록 데이터를 전달
+  }
+
   render() {
     const { classes } = this.props;
 
@@ -75,7 +76,9 @@ class App extends Component {
           </TableHead>
 
           <TableBody>
-            { customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); }) }
+            {/* 만약 this.state.customers의 값이 존재하는 경우 실행, 아닐 경우 공백을 출력(서버가 데이터를 바로 못 받는 경우가 있으므로 예외처리를 해줌) */}
+            { this.state.customers ? this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); 
+            }) : ""}
           </TableBody>
         </Table>
 
--- server.js
+++ server.js
@@ -3,13 +3,43 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;  // 5000포트
 
-app.use(bodyParser.json());
+app.use(bodyParser.json());  // json을 통해 데이터 주고 받음
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// api 내용: 서버에 접속하는 사용자(클라이언트) 입장에서 api 경로의 hello로 접속하면
-// 하나의 메시지를 사용자에게 출력 (서버 켠 뒤, http://localhost:5000/api/hello 경로로 확인해야 함)
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+// api 내용: 서버에 접속하는 사용자(클라이언트) 입장에서 api 경로로 접속하면 메시지를 사용자에게 출력
+// (서버 켠 뒤, http://localhost:5000/api/customers 경로로 확인해야 함)
+
+// 클라이언트가 저 경로로 접속하면 고객 정보가 있는 배열 데이터를 json 형식으로 반환
+// * 유효한 데이터인지 확인 가능한 사이트: https://jsonlint.com/
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '이름1',
+            'birthday': '생일1',
+            'gender': '성별1',
+            'job': '직업1'
+        },
+
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '이름2',
+            'birthday': '생일2',
+            'gender': '성별2',
+            'job': '직업2'
+        },
+
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '이름3',
+            'birthday': '생일3',
+            'gender': '성별3',
+            'job': '직업3'
+        }
+    ]);
 });
 
 // 따옴표('')가 아니라 숫자 1 옆의 ``를 넣어줘야 동작함
